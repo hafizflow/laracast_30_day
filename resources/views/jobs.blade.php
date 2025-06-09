6fa652dc16d60b7f3e45ae1a971c2a9eb92ddb12
hafizflow <hafizur.rahman.cs@gmail.com>
2025-06-09 22:01:46 +0600

    Day 14

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -18,5 +18,9 @@
                 </div>
             </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/jobs/{id}', function ($id) {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->simplePaginate(3);
 
     return view('jobs', [
         'jobs' => $jobs
